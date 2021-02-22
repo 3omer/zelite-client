1b69daf6df1802e145cb344747226a367da50a19
Omar M. Osman <omarosmandev@gmail.com>
2021-02-22 02:26:39 +0200

    use SwitchList

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,21 +1,16 @@
 <template>
-  <div class="p-2 my-2 bg-white shadow rounded-lg">
-    <h4>
-      <span class="badge badge-dark px-3 mb-2">Switches</span>
-    </h4>
-    <div id="switchList" class="mx-0 mx-md-2">
-        <SwitchDevice/>
-    </div>
-  </div>
+<div id="dashboard">
+    <SwitchList/>
+</div>
 </template>
 
 <script>
-import SwitchDevice from "../components/SwitchDevice"
+import SwitchList from "../components/SwitchList"
 
 export default {
   name: "Dashboard",
   components: {
-    SwitchDevice
+    SwitchList
   }
 };
 </script>
