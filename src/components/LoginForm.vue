0e8c8b4caa2a0cae3e40d8f106a5dd5cd1cb8136
Omar M. Osman <omarosmandev@gmail.com>
2021-02-23 23:57:37 +0200

    add login form view

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <Card>
+      <h3>Login</h3>
+      <hr>
+      <form id="loginForm" method="POST" novalidate>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="email" name="email" value=""
+                        placeholder="Email" required>
+                    <div class="invalid-feedback"></div>
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" id="password" name="password" value=""
+                        placeholder="Password" required>
+                    <div class="invalid-feedback"></div>
+                </div>
+                
+                <div class="border-top p-2 text-center">
+                    <button type="submit" class="btn btn-sm btn-primary m-3">Login</button>
+                    <router-link class="btn btn-sm btn-outline-primary" to="signup">I don't have account</router-link>
+                </div>
+            </form>
+  </Card>
+</template>
+
+<script>
+import Card from "./Card.vue"
+export default {
+    name: 'LoginForm',
+    components: {
+        Card
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
-import Signup from "../views/Signup.vue";
+import Signup from "../views/Signup.vue"
+import Login from '../views/Login.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -20,6 +22,11 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: Signup
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
     path: '/about',
