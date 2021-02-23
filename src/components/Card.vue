bea0cdd2211ba2306ef1c633c452406b485f9dd6
Omar M. Osman <omarosmandev@gmail.com>
2021-02-23 23:36:44 +0200

    add a card component

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-2 rounded-3 shadow-sm bg-white">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Card'
+}
+</script>
+
+<style>
+
+</style>
