0b93fe10e2cf3157baac6ce8f05cf0e8827fc95a
Omar M. Osman <omarosmandev@gmail.com>
2021-02-23 23:37:19 +0200

    contain the form inside a Card

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,64 +1,68 @@
 <template>
-  <div class="p-2 rounded-3 shadow-sm bg-white">
-      <h3>Create account</h3>
-      <hr>
+  <Card>
+    <h3>Create account</h3>
+    <hr />
     <div class="alert alert-danger">Error Message</div>
-        <form id="registerForm" method="POST">
-          <div class="form-group">
-            <input
-              type="text"
-              class="form-control"
-              id="username"
-              name="username"
-              value
-              placeholder="Username"
-              required
-            />
-            <div class="invalid-feedback"></div>
-          </div>
+    <form id="registerForm" method="POST">
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          id="username"
+          name="username"
+          value
+          placeholder="Username"
+          required
+        />
+        <div class="invalid-feedback"></div>
+      </div>
 
-          <div class="form-group">
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              name="email"
-              value
-              placeholder="Email"
-              required
-            />
-            <div class="invalid-feedback"></div>
-          </div>
+      <div class="form-group">
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          name="email"
+          value
+          placeholder="Email"
+          required
+        />
+        <div class="invalid-feedback"></div>
+      </div>
 
-          <div class="form-group">
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              name="password"
-              value
-              placeholder="Password"
-              required
-            />
-            <div class="invalid-feedback"></div>
-          </div>
-          <div class="border-top p-2">
-            <button type="submit" class="btn btn-sm btn-primary m-3">Register</button>
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          name="password"
+          value
+          placeholder="Password"
+          required
+        />
+        <div class="invalid-feedback"></div>
+      </div>
+      <div class="border-top p-2">
+        <button type="submit" class="btn btn-sm btn-primary m-3">Register</button>
 
-            <a class="btn btn-sm btn-outline-primary" href="/login">I have account</a>
-          </div>
-        </form>
-  </div>
+        <a class="btn btn-sm btn-outline-primary" href="/login">I have account</a>
+      </div>
+    </form>
+  </Card>
 </template>
 
 <script>
+import Card from "./Card.vue"
 export default {
-    name: 'SignupForm'
+  name: "SignupForm",
+  components: {
+      Card
+  }
 };
 </script>
 
 <style>
 .form-group {
-    margin-bottom: 1em;
+  margin-bottom: 1em;
 }
 </style>
