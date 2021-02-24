83ec69c43b625745eb35280da12aa7be33cbdc9c
Omar M. Osman <omarosmandev@gmail.com>
2021-02-24 22:34:02 +0200

    handle loggin

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,16 +1,16 @@
 <template>
   <Form>
     <template v-slot:title>Login</template>
-    <template v-slot:errors>smth went wrong</template>
+    <template v-if="error" v-slot:errors>{{error}}</template>
     <template v-slot:form>
-      <form id="loginForm" method="POST" novalidate>
+      <form @submit.prevent="onSubmit" id="loginForm" method="POST" novalidate>
         <div class="form-group">
           <input
+            v-model="email"
             type="text"
             class="form-control"
             id="email"
             name="email"
-            value
             placeholder="Email"
             autocomplete="email"
             required
@@ -20,6 +20,7 @@
 
         <div class="form-group">
           <input
+            v-model="password"
             type="password"
             class="form-control"
             id="password"
@@ -42,9 +43,33 @@
 </template>
 
 <script>
-import Form from "./Form";
+import Form from "./Form"
+import { login } from '../services/api'
+
 export default {
   name: "LoginForm",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: null
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('LoginForm:onSubmit()')
+      login(this.email, this.password)
+      .then(user => {
+        console.log('login passed');
+        this.error = null
+        this.$store.commit('SET_USER', user)
+      })
+      .catch(error => {
+        console.error('login failed', error);
+        this.error = error
+      })
+    },
+  },
   components: {
     Form
   }
