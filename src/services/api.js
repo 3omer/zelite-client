cd5f391cc8df481e39538f6b0851708b40815c2d
Omar M. Osman <omarosmandev@gmail.com>
2021-02-24 22:18:27 +0200

    handle login call

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,10 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhsot:5000/api/v1/'
+const BASE_URL = 'http://localhost:5000/api/v1'
 
 // config axios
 axios.defaults.baseURL = BASE_URL
-
-// Add a response interceptor to handle common errors
-axios.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-}, function (err) {
-    if (err.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        const msg = err.response.data ?
-            err.res.data.error ||
-            err.res.data.msg :
-            'Something went wrong'
-
-        Promise.reject(Error(msg))
-    } else if (err.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest
-        console.error(err.request)
-        // default error message
-        Promise.reject(Error('Something went wrong :('))
-    }
-})
+axios.defaults.timeout = 10000
 
 
 /*
@@ -38,9 +15,8 @@ axios.interceptors.response.use(function (response) {
 // all endpoints relatively to base
 const ENDPOINTS = {
     signup: '/register',
-    login: '/users/login',
-    logout: '/users/logout',
-    profile: '/users/me',
+    login: '/login',
+    logout: '/logout',
     devices: '/devices'
 }
 
@@ -67,8 +43,17 @@ const signup = async (username, email, password) => {
 const login = async (email, password) => {
     return axios.post(ENDPOINTS.login, { email, password })
         .then(res => {
-            // handle success
-            res.data
+            // return response payload
+            return res.data
+        })
+        .catch(err => {
+            // return a user friendly message to ui
+            let message = 'Something went Wrong'
+            console.error(err)
+            if (err.response) {
+                message = err.response.data.error || message
+            }
+            return Promise.reject(message)
         })
 }
 
