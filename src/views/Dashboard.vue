0d7c555e6ec3c656b1f247761b76ddea2a3cc52d
Omar M. Osman <omarosmandev@gmail.com>
2021-02-26 06:52:01 +0200

    fire data loading

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,20 +1,34 @@
 <template>
-<div id="dashboard">
-    <SensorList/>
-    <SwitchList/>
-</div>
+  <div id="dashboard">
+    <SensorList />
+    <SwitchList />
+  </div>
 </template>
 
 <script>
-import SwitchList from "../components/SwitchList"
-import SensorList from "../components/SensorsList"
+import { getDevices } from "../services/api";
+import SwitchList from "../components/SwitchList";
+import SensorList from "../components/SensorsList";
 
 export default {
   name: "Dashboard",
   components: {
     SwitchList,
     SensorList
-  }
+  },
+  created() {
+    // fetch user devices
+    console.log("Dashboard:created() - loading data");
+
+    getDevices(this.$store.getters.token)
+      .then(devices => {
+        console.log("Devices loaded - count", devices.length);
+        this.$store.commit("SET_DEVICES", devices);
+      })
+      .catch(err => {
+        console.log("Loading data failed", err);
+      });
+  },
 };
 </script>
 
