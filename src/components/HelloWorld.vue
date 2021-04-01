4b781d9769a73dda7454c12680414f98d23901e6
Omar M. Osman <omarosmandev@gmail.com>
2021-04-01 21:04:34 +0200

    lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,22 @@
 <template>
   <Card class="bg-light p-2 d-flex flex-column">
-    <router-link class="btn btn-lg btn-primary m-2" to="signup">
-      <i class="bi bi-person-lines-fill"></i>Signup
+    <router-link
+      class="btn btn-lg btn-primary m-2"
+      to="signup"
+    >
+      <i class="bi bi-person-lines-fill" />Signup
     </router-link>
-    <router-link class="btn btn-lg btn-outline-primary m-2" to="login">
-      <i class="bi bi-box-arrow-in-right"></i>Login
+    <router-link
+      class="btn btn-lg btn-outline-primary m-2"
+      to="login"
+    >
+      <i class="bi bi-box-arrow-in-right" />Login
     </router-link>
-    <router-link class="btn btn-lg btn-secondary m-2" to="about">
-      <i class="bi bi-question-circle"></i>Help
+    <router-link
+      class="btn btn-lg btn-secondary m-2"
+      to="about"
+    >
+      <i class="bi bi-question-circle" />Help
     </router-link>
   </Card>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,26 @@
 <template>
   <Card class="bg-light p-2 d-flex flex-column">
     <h5>Reach</h5>
-    <a class="btn btn-lg btn-outline-secondary m-2" href="#" target="blank">
-      <i class="bi bi-envelope-open-fill"></i>omarosmandev@gmail.com
+    <a
+      class="btn btn-lg btn-outline-secondary m-2"
+      href="#"
+      target="blank"
+    >
+      <i class="bi bi-envelope-open-fill" />omarosmandev@gmail.com
     </a>
-    <a class="btn btn-lg btn-dark m-2" href="https://github.com/3omer/zelite" target="blank">
-      <i class="bi bi-github"></i>Zelite Platform
+    <a
+      class="btn btn-lg btn-dark m-2"
+      href="https://github.com/3omer/zelite"
+      target="blank"
+    >
+      <i class="bi bi-github" />Zelite Platform
     </a>
-    <a class="btn btn-lg btn-primary m-2" href="https://fb.com/3omermhmd" target="blank">
-      <i class="bi bi-facebook"></i>Omer M. Osman
+    <a
+      class="btn btn-lg btn-primary m-2"
+      href="https://fb.com/3omermhmd"
+      target="blank"
+    >
+      <i class="bi bi-facebook" />Omer M. Osman
     </a>
   </Card>
 </template>
