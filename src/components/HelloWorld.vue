fe8498b8fd62b83c1af799a9344016c0914cfde1
Omar M. Osman <omarosmandev@gmail.com>
2021-04-01 20:56:24 +0200

    home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="hello">
-    Hello World
-  </div>
+  <Card class="bg-light p-2 d-flex flex-column">
+    <router-link class="btn btn-lg btn-primary m-2" to="signup">
+      <i class="bi bi-person-lines-fill"></i>Signup
+    </router-link>
+    <router-link class="btn btn-lg btn-outline-primary m-2" to="login">
+      <i class="bi bi-box-arrow-in-right"></i>Login
+    </router-link>
+    <router-link class="btn btn-lg btn-secondary m-2" to="about">
+      <i class="bi bi-question-circle"></i>Help
+    </router-link>
+  </Card>
 </template>
-
 <script>
+import Card from "../components/Card";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      client: undefined
+    };
+  },
+  components: {
+    Card
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
