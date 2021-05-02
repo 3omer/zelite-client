2de390db1673630f3881bce38512e7dc1cd3e70c
Omar M. Osman <omarosmandev@gmail.com>
2021-05-02 08:13:08 +0200

    update api call and display validaton feedbacks

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,60 +3,68 @@
     <template v-slot:title>
       <h3>Create Account</h3>
     </template>
-    <template
-      v-if="error"
-      v-slot:errors
-    >
+    <template v-if="error" v-slot:errors>
       {{ error }}
     </template>
     <template v-slot:form>
-      <form
-        @submit.prevent="onSubmit"
-        id="registerForm"
-        method="POST"
-      >
+      <form id="registerForm" method="POST" @submit.prevent="onSubmit">
         <div class="form-group">
           <input
+            id="username"
             v-model="username"
             type="text"
             class="form-control"
-            id="username"
+            :class="{
+              'is-invalid': validationFeedbacks.username != ''
+            }"
             name="username"
             value
             placeholder="Username"
             required
-          >
-          <div class="invalid-feedback" />
+          />
+          <div class="invalid-feedback text-danger">
+            {{ validationFeedbacks.username }}
+          </div>
         </div>
 
         <div class="form-group">
           <input
+            id="email"
             v-model="email"
             type="email"
             class="form-control"
-            id="email"
+            :class="{
+              'is-invalid': validationFeedbacks.email != ''
+            }"
             name="email"
             value
             placeholder="Email"
             autocomplete="email"
             required
-          >
-          <div class="invalid-feedback" />
+          />
+          <div class="invalid-feedback text-danger">
+            {{ validationFeedbacks.email }}
+          </div>
         </div>
 
         <div class="form-group">
           <input
+            id="password"
             v-model="password"
             type="password"
             class="form-control"
-            id="password"
+            :class="{
+              'is-invalid': validationFeedbacks.password != ''
+            }"
             name="password"
             value
             placeholder="Password"
             autocomplete="new-password"
             required
-          >
-          <div class="invalid-feedback" />
+          />
+          <div class="invalid-feedback">
+            {{ validationFeedbacks.password }}
+          </div>
         </div>
         <div class="border-top p-2">
           <eleActionBtn
@@ -66,10 +74,7 @@
             class="btn-primary mx-2"
             type="submit"
           />
-          <router-link
-            to="login"
-            class="btn btn-outline-primary"
-          >
+          <router-link to="login" class="btn btn-outline-primary">
             I have account
           </router-link>
           <!-- <a class="btn btn-outline-primary" href="/login">I have account</a> -->
@@ -80,44 +85,67 @@
 </template>
 
 <script>
-import Form from "./Form.vue";
-import eleActionBtn from "./ele-action-btn";
+import Form from './Form.vue'
+import eleActionBtn from './ele-action-btn'
 
-import { signup } from "../services/api";
+import { signup } from '../services/api'
 
 export default {
-  name: "SignupForm",
-  data() {
-    return {
-      username: "",
-      email: "",
-      password: "",
-      error: "",
-      isDisabled: false
-    };
-  },
-  methods: {
-    onSubmit() {
-      console.log("SignupForm:onSubmit()");
-      this.isDisabled = true
-      this.error = ""
-      signup(this.username, this.email, this.password)
-        .then(data => {
-          console.log("signup passed", { data });
-          this.error = "";
-          this.$router.push('login')
-        })
-        .catch(err => {
-          this.error = err;
-          this.isDisabled = false
-        });
-    }
-  },
+  name: 'SignupForm',
   components: {
     Form,
     eleActionBtn
+  },
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      error: '',
+      isDisabled: false,
+      validationErrors: {} // { username: ['error1', ..], password: ['error2'...] }
+    }
+  },
+  computed: {
+    validationFeedbacks() {
+      return {
+        username: ''.concat(this.validationErrors.username || ''),
+        email: ''.concat(this.validationErrors.email || ''),
+        password: ''.concat(this.validationErrors.password || '')
+      }
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      console.log('SignupForm:onSubmit()')
+      // disable submit btn
+      this.isDisabled = true
+      // clear top error message
+      this.error = ''
+      // clear previous validtion errors
+      this.validationErrors = {}
+      // api call
+      signup(this.username, this.email, this.password)
+        .then(data => {
+          console.log('signup passed', { data })
+          this.error = ''
+          this.$router.push('login')
+        })
+        .catch(err => {
+          // unlock submit btn
+          this.isDisabled = false
+          if (err.statusCode === 400) {
+            // validation error
+            this.validationErrors = err.data.messages
+          } else if (err.statusCode === -1) {
+            // server not reached or doesn't reply
+            this.error = err.mesaage
+          }
+        })
+    }
   }
-};
+}
 </script>
 
 <style>
--- src/services/api.js
+++ src/services/api.js
@@ -1,66 +1,70 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.VUE_APP_BACKEND_HOST || 'http://localhost:5000/api/v1'
+const BASE_URL =
+  process.env.VUE_APP_BACKEND_HOST || 'http://localhost:5000/api/v1'
 
 // config axios
 axios.defaults.baseURL = BASE_URL
 // axios.defaults.timeout = 30000
 
-
 /*
-* API operations
-* Docs: https://
-*/
+ * API operations
+ * Docs: https://
+ * all http relevant stuff lives here
+ * all the api operatoin returns promises
+ */
 
 // all endpoints relatively to base
 const ENDPOINTS = {
-    signup: '/register',
-    login: '/login',
-    logout: '/logout',
-    devices: '/devices',
-    MQTTConfig: '/mqtt'
+  signup: '/register',
+  login: '/login',
+  logout: '/logout',
+  devices: '/devices',
+  MQTTConfig: '/mqtt'
 }
 
 /**
  * Submit user details
- * @param {string} username 
- * @param {string} email 
- * @param {string} password 
- * @returns {Promise} User object when fullfiled. Error with usr-friendly message on rejection
+ * @param {string} username
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise.resolve} data {}
+ * @returns {Promise.reject} error {messages: [...]}
  */
 const signup = async (username, email, password) => {
-    return axios.post(ENDPOINTS.signup, { username, email, password })
-        .then(res => {
-            return res.data
-        })
-        .catch(err => {
-            console.error(err)
-            return Promise.reject(axiosErrorToMsg(err))
-        })
+  return axios
+    .post(ENDPOINTS.signup, { username, email, password })
+    .then(res => {
+      return res.data
+    })
+    .catch(error => {
+      return Promise.reject(handleRequestError(error))
+    })
 }
 
 /**
  * login and retrive a jwt token
- * @param {email} email 
- * @param {password} password 
- * @returns {Promise} 
+ * @param {email} email
+ * @param {password} password
+ * @returns {Promise}
  */
 const login = async (email, password) => {
-    return axios.post(ENDPOINTS.login, { email, password })
-        .then(res => {
-            // return response payload
-            return res.data
-        })
-        .catch(err => {
-            console.error(err)
-            return Promise.reject(axiosErrorToMsg(err))
-        })
+  return axios
+    .post(ENDPOINTS.login, { email, password })
+    .then(res => {
+      // return response payload
+      return res.data
+    })
+    .catch(err => {
+      console.error(err)
+      return Promise.reject(handleRequestError(err))
+    })
 }
 
-const authHeader = (token) => {
-    return {
-        "Authorization": `Bearer ${token}`
-    }
+const authHeader = token => {
+  return {
+    Authorization: `Bearer ${token}`
+  }
 }
 
 /**
@@ -68,95 +72,108 @@ const authHeader = (token) => {
  * @param {string} token current user's token
  * @returns {Promise} when resolved it return the server message, or user friendly message on rejection
  */
-const logout = (token) => {
-    return axios.post(ENDPOINTS.logout, {}, { headers: authHeader(token) })
-        .then(res => {
-            return res.data.message || "You logged out"
-        })
-        .catch(err => {
-            console.error(err)
-            return Promise.reject(axiosErrorToMsg(err))
-        })
-}
-
-
-const getDevices = (token) => {
-    return axios.get(ENDPOINTS.devices, { headers: authHeader(token) })
+const logout = token => {
+  return axios
+    .post(ENDPOINTS.logout, {}, { headers: authHeader(token) })
     .then(res => {
-        return res.data
+      return res.data.message || 'You logged out'
     })
     .catch(err => {
-        console.error(err)
-        return Promise.reject(axiosErrorToMsg(err))
+      console.error(err)
+      return Promise.reject(handleRequestError(err))
+    })
+}
+
+const getDevices = token => {
+  return axios
+    .get(ENDPOINTS.devices, { headers: authHeader(token) })
+    .then(res => {
+      return res.data
+    })
+    .catch(err => {
+      console.error(err)
+      return Promise.reject(handleRequestError(err))
     })
 }
 
 const postDevice = (token, device) => {
-    return axios.post(ENDPOINTS.devices, device, { headers: authHeader(token) })
+  return axios
+    .post(ENDPOINTS.devices, device, { headers: authHeader(token) })
     .then(res => {
-        return res.data
+      return res.data
     })
     .catch(err => {
-        console.error(err)
-        return Promise.reject(axiosErrorToMsg(err))
-        
+      console.error(err)
+      return Promise.reject(handleRequestError(err))
     })
 }
 
 const deleteDevice = (token, deviceKey) => {
-    const url = ENDPOINTS.devices + "/" + deviceKey
-    return axios.delete(url, { headers: authHeader(token) } )
-    .catch(err => {
-        console.error(err)
-        return Promise.reject(axiosErrorToMsg(err))
-    })
+  const url = ENDPOINTS.devices + '/' + deviceKey
+  return axios.delete(url, { headers: authHeader(token) }).catch(err => {
+    console.error(err)
+    return Promise.reject(handleRequestError(err))
+  })
 }
 
-const getMQTTConfig = (token) => {
-    return axios.get(ENDPOINTS.MQTTConfig, { headers: authHeader(token) })
+const getMQTTConfig = token => {
+  return axios
+    .get(ENDPOINTS.MQTTConfig, { headers: authHeader(token) })
     .then(res => {
-        return res.data
+      return res.data
     })
     .catch(err => {
-        console.error(err)
-        return Promise.reject(axiosErrorToMsg(err))
+      console.error(err)
+      return Promise.reject(handleRequestError(err))
     })
 }
 
 const postMQTTCred = (token, cred) => {
-    return axios.post(ENDPOINTS.MQTTConfig, cred, { headers: authHeader(token) })
+  return axios
+    .post(ENDPOINTS.MQTTConfig, cred, { headers: authHeader(token) })
     .then(res => {
-        return res.data
+      return res.data
     })
     .catch(err => {
-        console.error(err)
-        Promise.reject(axiosErrorToMsg(err))
-        
+      console.error(err)
+      Promise.reject(handleRequestError(err))
     })
 }
 
 /**
- * transofrm api calls errors to a userfriendly message
+ * process HTTP request/response erorrs and return a simple error object
+ * with the properties
+ * statusCode: http response code, or -1 if the equest failed
+ * data: response data .. eg { status: ... , messages:[...] }
+ * message: if the request failed
  * @param {Error} err Axios error object
  */
-const axiosErrorToMsg = (err) => {
-    // return a user friendly message to ui
-    console.error('API-Request.error: ', err)
-    
-    let message = 'Something went Wrong'
-    if (err.response) {
-        message = err.response.data.error || message
-    }
-    return message
+const handleRequestError = err => {
+  // return a user friendly message(s) to vue component
+  const simpleError = {}
+  if (err.response && err.response.data) {
+    // the backend is using only two properties to descripe
+    // errors; 'message' and 'messages' in case of validation errors
+    simpleError.data = err.response.data
+    simpleError.statusCode = err.response.status
+    return simpleError
+  } else if (err.request) {
+    // properly network error
+    simpleError.statusCode = -1
+    simpleError.message = 'It seems like you are not connected !'
+    return simpleError
+  } else {
+    throw err
+  }
 }
 
 export {
-    signup,
-    login,
-    logout,
-    getDevices,
-    postDevice,
-    deleteDevice,
-    getMQTTConfig,
-    postMQTTCred
+  signup,
+  login,
+  logout,
+  getDevices,
+  postDevice,
+  deleteDevice,
+  getMQTTConfig,
+  postMQTTCred
 }
